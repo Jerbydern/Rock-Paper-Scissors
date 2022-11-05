bd5701c374fb26843d01e86615d83debf7966fe2
Elwyn <elwynfoote@gmail.com>
2022-11-05 14:43:58 -0400

    Add primary game function

--- RPS.js
+++ RPS.js
@@ -1,12 +1,54 @@
 //Rock Paper Scissors
 
 function compChoice() {
-	choices = ['Rock','Paper','Scissors']
+	choices = ['rock','paper','scissors']
 	dex = Math.floor(Math.random() * 3)
 	return choices[dex]
 }
 
 function round(playerSel, compSel) {
-	prompt('Make your choice', 'rock')
+	playerSel = playerSel.toLowerCase()
+	if (playerSel === 'scissors') {
+		if (compSel === 'rock') {
+			return 'Rock beats Scissors! You lose, bitch!'
+		}
+		if (compSel === 'paper'){
+			return 'Scissors... beats... paper... You win. :('
+		}
+		if (compSel === 'scissors'){
+			return "It's a tie!"
+		}
+	}
+	if (playerSel === 'paper') {
+		if (compSel === 'rock') {
+			return 'Paper beats Rock. That means you win.'
+		}
+		if (compSel === 'paper'){
+			return 'Nothing happens. We tie. >:I'
+		}
+		if (compSel === 'scissors'){
+			return "I won! Scissors beats Paper! HA!"
+		}
+	}
+	if (playerSel === 'rock') {
+		if (compSel === 'rock') {
+			return 'Rock and Rock... hm...'
+		}
+		if (compSel === 'paper'){
+			return 'Rock LOSES to Paper. Hehehehehe'
+		}
+		if (compSel === 'scissors'){
+			return "Rock beats Scissors."
+		}
+	}
 }
+
+function game() {
+	for (let i = 0 ; i < 5 ; i++) {
+		result = round(prompt('Make your choice', 'Rock'), compChoice())
+		console.log("Round " + (i + 1) + ": " + result)
+	}
+}
+game()
+
 
