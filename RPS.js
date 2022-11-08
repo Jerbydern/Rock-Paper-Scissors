034afc21e98d30f56a81c4bafc04c99b02fae6fc
Elwyn <elwynfoote@gmail.com>
2022-11-08 18:50:26 -0500

    Set up full UI functionality

--- RPS.js
+++ RPS.js
@@ -1,6 +1,30 @@
 //Rock Paper Scissors
 
 let buttons = document.querySelectorAll('button');
+let outcome = document.querySelector('.outcome')
+let playerWins = 0
+let gameOver = false
+let winMessage = 'YOU WIN. I WILL HAVE MY REVENGE'
+let loseMessage = 'I WIN! HAHAHAHAHAHAHAH!!!!'
+let playerScoreDisp = document.querySelector('#playerScoreDisp')
+let compScoreDisp = document.querySelector('#compScoreDisp')
+let playerWinFunc = () => {
+	playerWins += 1
+	playerScoreDisp.innerHTML = playerWins
+	if (playerWins >= 5) {
+		gameOver = true
+		return true
+	}
+}
+let compWinFunc = () => {
+	compWins += 1
+	compScoreDisp.innerHTML = compWins
+	if (compWins >= 5) {
+		gameOver = true
+		return true
+	}
+}
+let compWins = 0
 
 function compChoice() {
 	choices = ['rock','paper','scissors']
@@ -12,10 +36,20 @@ function round(playerSel, compSel) {
 	playerSel = playerSel.toLowerCase()
 	if (playerSel === 'scissors') {
 		if (compSel === 'rock') {
+			if (compWinFunc()) {
+				return loseMessage
+			}
+			else {
 			return 'Rock beats Scissors! You lose, bitch!'
+			}
 		}
 		if (compSel === 'paper'){
-			return 'Scissors... beats... paper... You win. :('
+			if (playerWinFunc()){
+				return winMessage
+			}
+			else {
+				return 'Scissors... beats... paper... You win. :('
+			}
 		}
 		if (compSel === 'scissors'){
 			return "It's a tie!"
@@ -23,13 +57,23 @@ function round(playerSel, compSel) {
 	}
 	if (playerSel === 'paper') {
 		if (compSel === 'rock') {
-			return 'Paper beats Rock. That means you win.'
+			if (playerWinFunc()){
+				return winMessage
+			}
+			else {
+				return 'Paper beats Rock. That means you win.'
+			}
 		}
 		if (compSel === 'paper'){
 			return 'Nothing happens. We tie. >:I'
 		}
 		if (compSel === 'scissors'){
+			if (compWinFunc()) {
+				return loseMessage
+			}
+			else {
 			return "I won! Scissors beats Paper! HA!"
+			}
 		}
 	}
 	if (playerSel === 'rock') {
@@ -37,17 +81,32 @@ function round(playerSel, compSel) {
 			return 'Rock and Rock... hm...'
 		}
 		if (compSel === 'paper'){
+			if (compWinFunc()) {
+				return loseMessage
+			}
+			else {
 			return 'Rock LOSES to Paper. Hehehehehe'
+			}
 		}
 		if (compSel === 'scissors'){
-			return "Rock beats Scissors."
+			if (playerWinFunc()) {
+				return winMessage
+			}
+			else {return "Rock beats Scissors."}
 		}
 	}
 }
 
 buttons.forEach((button) => {
 	button.addEventListener('click', () => {
-		console.log(round(button.id, compChoice()))
+		if (gameOver === false){
+			let result = document.createElement('div');
+			result.innerHTML = round(button.id, compChoice())
+			if (gameOver === true){
+				result.style = "color:purple;"
+			}
+			outcome.appendChild(result)
+		}
 	})
 })
 
@@ -61,5 +120,3 @@ buttons.forEach((button) => {
 	}
 }
 game() */
-
-
